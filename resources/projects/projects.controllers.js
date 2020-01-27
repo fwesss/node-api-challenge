d64caf0f3b21a447c24c5c79e7f21c312bc1abc9
Westley Feller <feller.westley@gmail.com>
2020-01-26 19:37:59 -0800

    Message if project has no actions

--- resources/projects/projects.controllers.js
+++ resources/projects/projects.controllers.js
@@ -4,7 +4,13 @@ import Projects, { getProjectActions } from './projects.model'
 const getManyProjectActions = async (req, res) => {
   try {
     const actions = await getProjectActions(req.params.id)
-    res.status(200).json(actions)
+    if (actions.length) {
+      res.status(200).json(actions)
+    } else {
+      res
+        .status(404)
+        .json({ message: 'There are no actions associated with this project' })
+    }
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'The information could not be retrieved.' })
